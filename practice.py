9dfa0b741a398f654e14e70261d6c9faa0471cb4
Sneha Bista <snehabista727@gmail.com>
2024-02-09 13:38:04 -0600

    added python file to test flask

---
+++ practice.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+ 
+@app.route('/home/')
+def home_world():
+    return 'Homepage'
+ 
+if __name__ == '__main__':
+    app.run()
+    
