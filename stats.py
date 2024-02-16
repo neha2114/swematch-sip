19147be83961718f6282d11a85d356d4aacf0fd0
Shreya <shreyamekala01@gmail.com>
2024-02-16 12:17:36 -0600

    started the app base

---
+++ stats.py
@@ -0,0 +1,7 @@
+def stats_page():
+    # Logic for the home page
+    return 'Stats oijfdoijf Page'
+
+if __name__ == '__main__':
+    # Run the home page logic if the file is executed directly
+    print(stats_page())
