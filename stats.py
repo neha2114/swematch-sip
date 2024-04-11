991a36f68d34ca444a39ef5a88cb3245aa02946d
neha2114 <neha.sapre2114@gmail.com>
2024-04-11 18:22:49 -0500

    stats page with title that reads from firebase

--- stats.py
+++ stats.py
@@ -1,7 +1,14 @@
+from firebase import firebase
+
+# initialize firebase url
+firebase = firebase.FirebaseApplication('https://test1-f1e04-default-rtdb.firebaseio.com/', None)
+
+# this function is passed to tutorial.py (main page)
 def stats_page():
-    # Logic for the home page
-    return 'Stats oijfdoijf Page'
+    # Logic for the stats page
+    result = firebase.get('/weights', None)
+    return 'Stats Page' + str(result)
 
 if __name__ == '__main__':
-    # Run the home page logic if the file is executed directly
+    # just for debugging, print stats page info to the console
     print(stats_page())
