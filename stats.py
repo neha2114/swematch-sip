1f2551918ff056baf54d29be7226966b1bc9389d
Shreya <shreyamekala01@gmail.com>
2024-04-21 17:17:07 -0500

    updated to website
    added navbar, formatted header, created containers for each category

--- stats.py
+++ stats.py
@@ -1,7 +1,32 @@
+#from firebase import firebase
+from flask import render_template
+import json
+
+# initialize firebase url
+#firebase = firebase.FirebaseApplication('https://test1-f1e04-default-rtdb.firebaseio.com/', None)
+
+# this function is passed to tutorial.py (main page)
 def stats_page():
-    # Logic for the home page
-    return 'Stats oijfdoijf Page'
+    # Logic for the stats page
+    #result = firebase.get('/weights', None)
+
+    # Define Plot Data 
+    labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+    ]
+ 
+    data = [0, 10, 15, 8, 22, 18, 25]
+ 
+    # Return the components to the HTML template 
+    return render_template('stat.html')
+    #return render_template('stat.html', weights=result)
+    #return 'Stats Page' + str(result)
 
 if __name__ == '__main__':
-    # Run the home page logic if the file is executed directly
+    # just for debugging, print stats page info to the console
     print(stats_page())
